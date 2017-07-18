1fad3d3dbfa59eb0bcb643dea4821564e3412feb
Jorge Schrauwen <sjorge@acheron.be>
2017-07-18 23:10:56 +0200

    tools - make get_setup_hook copy the script instead of symlinking

--- tools/git_setup_hook.sh
+++ tools/git_setup_hook.sh
@@ -4,7 +4,7 @@
 ####
 
 TOPDIR="$(git rev-parse --show-toplevel)"
-ln -s "${TOPDIR}/tools/.git-pre-commit" "${TOPDIR}/.git/hooks/pre-commit"
-ln -s "${TOPDIR}/tools/.git-post-checkout" "${TOPDIR}/.git/hooks/post-checkout"
+cp "${TOPDIR}/tools/.git-pre-commit" "${TOPDIR}/.git/hooks/pre-commit"
+cp "${TOPDIR}/tools/.git-post-checkout" "${TOPDIR}/.git/hooks/post-checkout"
 
 # vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
