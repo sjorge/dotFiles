f4e9d70d43240aa8233fc809961ffdbda1ef7e79
Jorge Schrauwen <sjorge@acheron.be>
2017-07-18 17:43:32 +0200

    tools - more pkgsrc preferences

--- tools/prefer_pkgsrc.sh
+++ tools/prefer_pkgsrc.sh
@@ -5,16 +5,28 @@
 ###
 
 PKGSRC_BIN=/opt/pkg/bin
-PKGSRC_WANT=(zsh rsync)
+PKGSRC_WANT=(
+  zsh
+  rsync
+  ssh
+  ssh-add
+  ssh-agent
+  ssh-keygen
+  ssh-keyscan
+  git
+)
 
 for tool in ${PKGSRC_WANT}; do
  local pkgsrc_path="${PKGSRC_BIN}/${tool}"
  local local_path="/usr/local/bin/${tool}"
 
  # skip if pkgsrc binary is not present
- [ ! -x "${pkgsrc_path}" ] && continue
+ if [ ! -x "${pkgsrc_path}" ]; then
+   [ -L "${local_path}" ] && rm "${local_path}"
+   continue
+ fi
 
- if [ ! -e "${local_path}" ] || [ -L ${local_path} ]; then
+ if [ ! -e "${local_path}" ] || [ -L "${local_path}" ]; then
    ln -sf "${pkgsrc_path}" "${local_path}"
  fi
 done
